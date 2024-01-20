64f9b556ac6db3c7a393a4afb90eb29231442ce6
0xTranqui <thinkandchill11@gmail.com>
2024-01-20 13:06:40 -0500

    registry 2 onchain

--- src/GenericRegistry2.sol
+++ src/GenericRegistry2.sol
@@ -6,6 +6,7 @@ import "solidity-bytes-utils/BytesLib.sol";
 import {IdRegistry} from "./IdRegistry.sol";
 import {DelegateRegistry} from "./DelegateRegistry.sol";
 import {Auth} from "./abstract/Auth.sol";
+import {IRenderer} from "./interfaces/IRenderer.sol";
 
 interface IStore {
     function initialize(uint256 userId, bytes32 uid, bytes calldata data) external;
@@ -14,68 +15,175 @@ interface IStore {
     function getWriteAccess(uint256 userId, bytes32 uid, bytes memory data) external returns (bool);
 }
 
-// interface ILogic {
-//     function initialize(uint256 userId,)
-// }
+/*
+    NOTE: Channel Store
 
-contract ChannelStore {
-    error OnlyRegistry();
+    - Allows an external address to initialize data (ex: uri) + access (ex: admin) for a given uri
+    - Permissionless write access, apps would want to know what their origins are to filter out events
+      related to infra they are running
+    - Initialize + Write calls need to be made by the same origin address
+
+*/
+contract ChannelStore is IStore {
+    enum Commands {
+        URI,
+        ADMIN
+    }
+    //
     error OnlyAdmin();
-    GenericRegistry constant public genericRegistry = 0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5;
-    mapping(bytes32 uid => string uri) public uriForUid;
-    mapping(bytes32 uid => address admin) public adminForUid;
+    //
+    event Initialize(address origin, uint256 userId, bytes32 uid, string uri, uint256 admin);
+    event Uri(address origin, uint256 userId, bytes32 uid, string uri);
+    event Admin(address origin, uint256 userId, bytes32 uid, uint256 admin);
+    //
+    mapping(address origin => mapping(bytes32 uid => string uri)) public uriForUid;        // this could be generic data
+    mapping(address origin => mapping(bytes32 uid => uint256 admin)) public adminForUid;   // this could be generic logic 
+    //
     function initialize(uint256 userId, bytes32 uid, bytes calldata data) external {
-        if (msg.sender != address(genericRegistry)) revert OnlyRegistry();
-        (string memory uri, address admin) = abi.decode(data, (string, address));
-        uriForUid[uid] = uri;
-        adminFor[uid] = adminForUid;
+        address sender = msg.sender;
+        (string memory uri, uint256 admin) = abi.decode(data, (string, uint256));
+        uriForUid[sender][uid] = uri;
+        adminForUid[sender][uid] = admin;
+        emit Initialize(sender, userId, uid, uri, admin);
     }
     // could add a command slicer to this to allow for multiple write pathways
     // can return abi.encoded(data for pathway) + the flag thats decoded to provide generic return
+    // NOTE: uid here = channel uid
     function write(uint256 userId, bytes32 uid, bytes calldata data) external {
-        if (adminForUid[uid] != userId) revert OnlyAdmin();
-        if (data[0:1] == 0) {
-            (string memory newUri) = abi.encode(data[1:], (string));
-            uriForUid[uid] = newUri;
+        address sender = msg.sender;
+        bytes memory dataCopy = data[0:1];
+        bytes1 dataCopySpec = bytes1(dataCopy);
+        uint8 commandFlag = uint8(dataCopySpec);
+        if (!_isAdmin(sender, userId, uid)) revert OnlyAdmin();
+        if (commandFlag == uint8(Commands.URI)) {
+            (string memory newUri) = abi.decode(data[1:], (string));
+            uriForUid[sender][uid] = newUri;
+            emit Uri(sender, userId, uid, newUri);
         } else {
-            (address newAdmin) = abi.encode(data[1:], (string));
-            adminForUid[uid] = newAdmin;            
+            (uint256 newAdmin) = abi.decode(data[1:], (uint256));
+            adminForUid[sender][uid] = newAdmin;            
+            emit Admin(sender, userId, uid, newAdmin);
         }
     }
+    // 
+    function _isAdmin(address sender, uint256 userId, bytes32 uid) internal view returns (bool) {
+        return adminForUid[sender][uid] == userId ? true : false;
+    }
+    //
+    function getReplaceAccess(uint256 userId, bytes32 uid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, uid);
+    }
+    function getWriteAccess(uint256 userId, bytes32 uid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, uid);
+    }    
+    //
+    function getAddAccess(uint256 userId, bytes32 uid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, uid);
+    }
+    function getRemoveAccess(uint256 userId, bytes32 uid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, uid);
+    }    
+    function channelUri(address origin, bytes32 uid) external view returns (string memory uri) {
+        uri = uriForUid[origin][uid];
+    }    
 }
 
 contract ItemStore {
-    error OnlyRegistry();
+    enum Commands {
+        ADMIN,
+        DATA,
+        ADD,
+        REMOVE
+    }
+    struct Channel {
+        address store;
+        bytes32 uid;
+        bytes data;
+    }    
+    //
     error OnlyAdmin();
-    GenericRegistry constant public genericRegistry = 0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5;
-    ChannelStore constant public channelStore = 0x15222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5;
-    // mapping(bytes32 uid => string uri) public uriForUid;
-    // mapping(bytes32 uid => address admin) public adminForUid;
-    // function initialize(uint256 userId, bytes32 uid, bytes calldata data) external {
-    //     if (msg.sender != address(genericRegistry)) revert OnlyRegistry();
-    //     (string memory uri, address admin) = abi.decode(data, (string, address));
-    //     uriForUid[uid] = uri;
-    //     adminFor[uid] = adminForUid;
-    // }
+    error No_Add_Access();
+    error No_Remove_Access();
+    //
+    event Initialize(address origin, uint256 userId, bytes32 itemUid, address pointer);
+    event Data(address origin, uint256 userId, bytes32 itemUid, address pointer);
+    event Admin(address origin, uint256 userId, bytes32 itemUid, uint256 admin);
+    event Add(address origin, uint256 userId, bytes32 itemUid, bytes32 channelUid);
+    event Remove(address origin, uint256 userId, bytes32 itemUid, bytes32 channelUid);
+    //
+    mapping(address origin => mapping(bytes32 item => address pointer)) public dataForItem;    
+    mapping(address origin => mapping(bytes32 item => uint256 admin)) public adminForItem; 
+    mapping(address origin => mapping(bytes32 item => mapping(bytes32 channel => uint256 userId))) addedItemToChannel;
+    //
+    function initialize(uint256 userId, bytes32 uid, bytes calldata data) external {
+        address sender = msg.sender;
+        address pointer = dataForItem[sender][uid] = SSTORE2.write(data);
+        adminForItem[sender][uid] = userId;
+        emit Initialize(sender, userId, uid, pointer);
+    }
+
     // could add a command slicer to this to allow for multiple write pathways
     // can return abi.encoded(data for pathway) + the flag thats decoded to provide generic return
-    // function write(uint256 userId, bytes32 uid, bytes calldata data) external {
-    //     if (adminForUid[uid] != userId) revert OnlyAdmin();
-    //     if (data[0:1] == 0) {
-    //         (string memory newUri) = abi.encode(data[1:], (string));
-    //         uriForUid[uid] = newUri;
-    //     } else {
-    //         (address newAdmin) = abi.encode(data[1:], (string));
-    //         adminForUid[uid] = newAdmin;            
-    //     }
-    // }
+    // NOTE: uid here = item uid
+    function write(uint256 userId, bytes32 uid, bytes calldata data) external {
+        address sender = msg.sender;
+        bytes memory dataCopy = data[0:1];
+        bytes1 dataCopySpec = bytes1(dataCopy);
+        uint8 commandFlag = uint8(dataCopySpec);
+        if (!_isAdmin(sender, userId, uid)) revert OnlyAdmin();
+        if (commandFlag == uint8(Commands.ADMIN)) {
+            (uint256 newAdmin) = abi.decode(data[1:], (uint256));
+            adminForItem[sender][uid] = newAdmin;            
+            emit Admin(sender, userId, uid, newAdmin);
+        } else if (commandFlag == uint8(Commands.DATA)) {
+            address pointer = dataForItem[sender][uid] = SSTORE2.write(data[1:]);
+            emit Data(sender, userId, uid, pointer);
+            /*
+                TODO: Integrate add functionality into DATA setup call as well
+            */
+        } else if (commandFlag == uint8(Commands.ADD)) {
+            (Channel[] memory channels) = abi.decode(data[1:], (Channel[]));
+            for (uint256 i; i < channels.length; ++i) {
+                if (!ChannelStore(channels[i].store).getAddAccess(userId, channels[i].uid, channels[i].data)) revert No_Add_Access(); 
+                addedItemToChannel[sender][uid][channels[i].uid] = userId;
+                emit Add(sender, userId, uid, channels[i].uid);
+            }
+        } else if (commandFlag == uint8(Commands.REMOVE)) {
+            (Channel memory channel) = abi.decode(data[1:], (Channel));
+            if (userId != addedItemToChannel[sender][uid][channel.uid]) {
+                if (ChannelStore(channel.store).getRemoveAccess(userId, channel.uid, channel.data)) {
+                    revert No_Remove_Access();
+                }
+            }      
+            delete addedItemToChannel[sender][uid][channel.uid];
+            emit Remove(sender, userId, uid, channel.uid);
+        }
+    }
+    // 
+    function _isAdmin(address sender, uint256 userId, bytes32 itemUid) internal view returns (bool) {
+        return adminForItem[sender][itemUid] == userId ? true : false;
+    }
+    //
+    function getReplaceAccess(uint256 userId, bytes32 itemUid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, itemUid);
+    }
+    function getWriteAccess(uint256 userId, bytes32 itemUid, bytes memory /*data*/) external view returns (bool) {
+        return _isAdmin(msg.sender, userId, itemUid);
+    }    
+    //
+    function itemUri(address origin, bytes32 itemUid) external view returns (string memory uri) {
+        bytes memory encodedBytes = SSTORE2.read(dataForItem[origin][itemUid]);
+        address renderer = BytesLib.toAddress(encodedBytes, 0);
+        bytes memory data = BytesLib.slice(encodedBytes, 20, (encodedBytes.length - 20));
+        uri = IRenderer(renderer).render(data);
+    }   
 }
 
 /**
  * @title GenericRegistry2
  * @author Lifeworld
  */
-contract GenericRegistry is Auth {
+contract GenericRegistry2 is Auth {
     struct Update {
         bytes32 uid;
         uint8 flag;
@@ -141,7 +249,7 @@ contract GenericRegistry is Auth {
             // should make writeTo the first check since will be more often            
             if (updates[i].flag == 0) {
                 // Extract logic module from uid data
-                IStore store = IStore(dataForUid[updates[i].uid]);
+                IStore store = IStore(storeForUid[updates[i].uid]);
                 // Check if user has access to replace store for uid
                 if (!store.getReplaceAccess(userId, updates[i].uid, updates[i].data)) revert No_Replace_Access(); 
                 // Extract newStore address             
@@ -150,7 +258,7 @@ contract GenericRegistry is Auth {
                 IStore(newStore).initialize(userId, updates[i].uid, updates[i].data[20:]);                                         
             } else { // WRITE TO BELOW
                 // Extract logic module from uid data
-                IStore store = IStore(dataForUid[updates[i].uid]);
+                IStore store = IStore(storeForUid[updates[i].uid]);
                 // Check if user has access to write to store
                 if (!store.getWriteAccess(userId, updates[i].uid, updates[i].data)) revert No_Write_Access();
                 // Write to store
