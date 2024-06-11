29cb5ed4f65c9947413c29b6e8c3d10fb8a1e0a0
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-06-11 17:12:36 -0500

    update testSuite with hardcoded passkeys for now

--- test/TestSuiteSetup.sol
+++ test/TestSuiteSetup.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.23;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 
 abstract contract TestSuiteSetup is Test {
     /*//////////////////////////////////////////////////////////////
@@ -14,14 +14,15 @@ abstract contract TestSuiteSetup is Test {
     Account public relayer = makeAccount("relayer");
     Account public user = makeAccount("user");
     Account public malicious = makeAccount("malicious");
+    uint256 passkeyPrivateKey = uint256(0x03d99692017473e2d631945a812607b23269d85721e0f370b8d3e7d29a874fd2);
+    bytes passkeyOwner =
+        hex"1c05286fe694493eae33312f2d2e0d0abeda8db76238b7a204be1fb87f54ce4228fef61ef4ac300f631657635c28e59bfb2fe71bce1634c81c65642042f6dc4d";
 
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
-    function setUp() public virtual {        
-    
-    }
+    function setUp() public virtual {}
 
     /*//////////////////////////////////////////////////////////////
                                  HELPERS
@@ -39,5 +40,5 @@ abstract contract TestSuiteSetup is Test {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
         sig = abi.encodePacked(r, s, v);
         assertEq(sig.length, 65);
-    }       
+    }
 }
