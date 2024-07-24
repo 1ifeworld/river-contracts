2272a2643b10b1895401013cd6b590ada87c254a
0xTranqui <max@lifeworld.co>
2024-07-24 02:45:58 -0400

    simplify tests

--- test/RiverRegistry/RiverRegistry.t.sol
+++ test/RiverRegistry/RiverRegistry.t.sol
@@ -36,35 +36,24 @@ contract RiverRegistryTest is RiverRegistryTestSuite {
         // start prank as untrusted caller
         vm.startPrank(malicious.addr);
 
-        uint256 migrationCutoff = riverRegistry.RID_MIGRATION_CUTOFF();
-
-        for (uint256 i; i < migrationCutoff; ++i) {
-            address randomAccount = randomAccount(i);
-            vm.expectRevert(abi.encodeWithSignature("Only_Trusted()"));
-            riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
-            break;
-        }
+        address randomAccount = randomAccount(1);
+        vm.expectRevert(abi.encodeWithSignature("Only_Trusted()"));
+        riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
+        assertEq(riverRegistry.idCount(), 0);
     }       
 
     function test_revert_dupCustody_prepMigrate() public {
         // start prank as trusted caller
         vm.startPrank(trusted.addr);
 
-        uint256 migrationCutoff = riverRegistry.RID_MIGRATION_CUTOFF();
-        address duplicate;
+        address randomAccount = randomAccount(1);
+        riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
 
-        for (uint256 i; i < migrationCutoff; ++i) {
-            address randomAccount = randomAccount(i);
-            if (i == 0) {
-                duplicate = randomAccount;
-            }
-            if (i == 1) {
-                vm.expectRevert(abi.encodeWithSignature("Has_Id()"));
-                riverRegistry.trustedPrepMigration(duplicate, recovery.addr);
-                break;
-            }        
-            riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
-        }
+        assertEq(riverRegistry.custodyOf(1), randomAccount);
+
+        vm.expectRevert(abi.encodeWithSignature("Has_Id()"));
+        riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
+        assertEq(riverRegistry.idCount(), 1);
     }       
 
     function test_revert_pastCutoff_prepMigrate() public {
@@ -72,14 +61,15 @@ contract RiverRegistryTest is RiverRegistryTestSuite {
         vm.startPrank(trusted.addr);
 
         uint256 migrationCutoff = riverRegistry.RID_MIGRATION_CUTOFF();
-        uint256 pastCutoff = migrationCutoff + 1;
 
-        for (uint256 i; i < pastCutoff; ++i) {
-            address randomAccount = randomAccount(i);
-            if (i == migrationCutoff) {
-                vm.expectRevert(abi.encodeWithSignature("Past_Migration_Cutoff()"));
-            }            
+        for (uint256 i; i < migrationCutoff; ++i) {
+            address randomAccount = randomAccount(i);     
             riverRegistry.trustedPrepMigration(randomAccount, recovery.addr);
         }
+
+        address anotherRandomAccount = randomAccount(migrationCutoff + 1);
+        vm.expectRevert(abi.encodeWithSignature("Past_Migration_Cutoff()"));
+        riverRegistry.trustedPrepMigration(anotherRandomAccount, recovery.addr);
+        assertEq(riverRegistry.idCount(), migrationCutoff);
     }     
 }
